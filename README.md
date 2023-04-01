10322b653a8af3c7d0a7920356b364ccf383a4f5
gmfractal <fractalnow@gmail.com>
2023-03-31 23:28:52 -0400

    improve explanation wording

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # array-map-parseint-explanation
-Explanation for JavaScript Array map and parseInt problem
 
-In March 2023, JamesQQuick posted a snippet of JavaScript code using the Array.prototype.map method and parseInt that returned a seemingly incorrect result (https://youtube.com/shorts/dXbR7sGFCMc?feature=share).
+## Explanation for JavaScript Array map and parseInt problem
 
-The problem is that running `["1", "10", "100"].map(parseInt)` will return `[1, NaN, 4]` instead of `[1, 10, 100]`, but why? 
+In March 2023, JamesQQuick posted a snippet of JavaScript code using the `Array.prototype.map` method and `parseInt` that returned a seemingly incorrect result (https://youtube.com/shorts/dXbR7sGFCMc?feature=share).
 
-The causes of the problem is explained here with examples.
+The problem is that running `["1", "10", "100"].map(parseInt)` will return `[1, NaN, 4]` instead of `[1, 10, 100]`, but why?
+
+The causes of the problem is explained here with examples. You should have a basic understanding of `Array.prototype.map` and `parseInt`, take a look at the links below to learn more about them.
 
 ## Further Reading
+
 - MDN page on `parseInt`: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
 - MDN page on `Array.prototype.map`: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
--- array-map-parseint-explanation.js
+++ array-map-parseint-explanation.js
@@ -6,24 +6,28 @@
 
 /**
  * PROBLEM STATEMENT: The problem begins with an array containing numbers in string form. The map method is used 
- * to map over every element of the array and passes the parseInt function as the callback. Normally, 
- * we would expect a new array to be logged to console that contains the number versions of the strings that 
- * were in the original array but that's not what we get here.
+ * to map over every element of the array and passes the parseInt function as the callback. In other words, the map
+ * method executes the callback function on each element of the array that it was called on and returns the results
+ * as a new array. Normally, we would expect a new array to be logged to console that contains the number versions of 
+ * the strings that were in the original array but that's not what we get here (see Example 1 below).
  */
 
 const numberStrings = ["1", "10", "100"]
 
+// Example 1
 console.log(numberStrings.map(parseInt));  // Expected: [1, 10, 100]
                                            // Actual: [1, NaN, 4]
 
-console.log(numberStrings.map((value) => parseInt(value))) // But this works fine and returns [1, 10, 100]
+// Example 2
+console.log(numberStrings.map((value) => parseInt(value))) // But this works fine and returns [1, 10, 100] but we'll
+                                                           // come back to this example later
 
 /**
  * The array that gets logged to console is very different than what is expected. The reason requires an 
  * understanding of the map method, parseInt, and how they interact in this situation. The map method actually
  * passes 3 arguments to its callback: the current array element, the current index, and a reference to the array 
- * that the map method was called on. So in the above code, map is passing more than just the current string element 
- * to the parseInt function. The arguments passed to parseInt for each element actually looks like this:
+ * that the map method was called on. So in Example 1 above, map is passing more than just the current string element 
+ * to the parseInt function. The arguments passed to parseInt for each element in Example 1 actually looks like this:
  */
 
 parseInt("1", 0, numberStrings); // 1st element
@@ -40,7 +44,8 @@ parseInt("100", 2, numberStrings); // 3rd element
  * 
  * Now we can understand the cause of the problem. Because the map method is passing the index number to 
  * parseInt as well, the parseInt function treats that index number as the radix of the string to be parsed.
- * This is how parseInt interprets the arguments that it receives for each element:
+ * This is how parseInt interprets the arguments that it receives for each element (it ignores the third argument
+ * which is not used by parseInt):
  */
 
 parseInt("1", 0, numberStrings); // 1st element: radix argument is 0, so parseInt defaults to radix 10 instead 
@@ -52,12 +57,12 @@ parseInt("100", 2, numberStrings); // 3rd element: radix argument is 2, so parse
                                    // and returns 4
 
 /** 
- * This also explains why wrapping parseInt in another function and then passing it to the map method as the 
- * callback will produce the expected results.
+ * This also explains why wrapping parseInt in another function, like in Example 2 above, and then passing it to the 
+ * map method as the callback will produce the expected results. Let's look at Example 2 again:
  */
 
 numberStrings.map((value) => parseInt(value)) // This works because the callback is an arrow function that only
                                               // passes the string number into parseInt without any radix argument
                                               // and returns the results. This allows parseInt to determine the 
-                                              // appropriate radix and in this case, it will use radix 10 and that
-                                              // aligns with our expectations.
+                                              // appropriate radix based on its own rules and in this case, it will 
+                                              // use radix 10 and that aligns with our expectations.
